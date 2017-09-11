d8dbcc5b00aab6b847a104bf313150b237bbaa31
Alex Nikanov <alekseynikanov.21@gmail.com>
2017-09-11 22:54:48 +0300

    Add files via upload

--- goodybot.py
+++ goodybot.py
@@ -8,7 +8,7 @@ class BotHandler:
         self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
  
-    def get_updates(self, offset=None, timeout=30):
+    def get_updates(self, offset=None, timeout=10):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
@@ -27,32 +27,18 @@ class BotHandler:
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
-            last_update = get_result[len(get_result)]
+            last_update = {'update_id':'None','message':{'text':'None', 'chat':{'id':'None'}}}
  
         return last_update
 
-    def get_random_message(self):
-        updates = self.get_updates()
-        randMsg = updates[random.randint(0, len(updates) - 1)]
-        i = 0
-
-        while ('goody' in randMsg['message']['text'] or 'гуди' in randMsg['message']['text']) and i < len(updates):
-            randMsg = updates[random.randint(0, len(updates) - 1)]
-            i+=1
-        
-        if i == len(updates):
-            return 'NaN'
-        else:
-            return randMsg['message']['text']
-
-goodyBot = BotHandler("406615403:AAEsQPhX9-rGqPd6ApJo7qYtlG5Wuq459s8")
-
-greetings = ('Hi', 'HI', 'Hey', "What's up?", "How RU?")
+goodyBot = BotHandler("416682801:AAF4QDGbapfRccmaA6xyR7YgTntovnBn0m0")
 
 def main():
     new_offset = None
-    TEST_MESSAGE = 'Hi'
+    TEST_MESSAGE = 'TEST_MESSAGE'
+    LENGTH_MESSAGES = 100
     used = []
+    allMsg = []
     
     while True:
         goodyBot.get_updates(new_offset)
@@ -60,18 +46,35 @@ def main():
         last_update = goodyBot.get_last_update()
  
         last_update_id = last_update['update_id']
+<<<<<<< HEAD
+        last_chat_text = last_update['message']['text'] or 'None'
+        last_chat_id = last_update['message']['chat']['id'] or 'None'
+        
+        if len(allMsg) < LENGTH_MESSAGES:
+            if last_chat_text != 'None' and not (last_chat_text in allMsg):
+                allMsg.append(last_chat_text)
+        else:
+            allMsg = allMsg[1:]
+            if last_chat_text != 'None' and not (last_chat_text in allMsg):
+                allMsg.append(last_chat_text)
+=======
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
         #last_chat_name = last_update['message']['chat']['first_name']
+>>>>>>> 0bace003da5e04f0b0b8e41fc4945b183a771767
 
-        message = goodyBot.get_random_message()
+        message = allMsg[random.randint(0, len(allMsg) - 1)]
 
-        if ('goody' in last_chat_text.lower() or 'гуди' in last_chat_text.lower().split()) and not (last_update_id in used):
-            if message == 'NaN':
+        if ('goody' in last_chat_text.lower().split() or 'гуди' in last_chat_text.lower().split() or
+            'goody ' in last_chat_text.lower() or ' goody' in last_chat_text.lower() or
+            'гуди ' in last_chat_text.lower() or ' гуди' in last_chat_text.lower()) and not (last_update_id in used):
+            if message == 'None':
                 goodyBot.send_message(last_chat_id, TEST_MESSAGE)
             else:
                 goodyBot.send_message(last_chat_id, message)
                 used.append(last_update_id)
+
+        new_offset = last_update_id + 1
 
 if __name__ == '__main__' :
 	try:
