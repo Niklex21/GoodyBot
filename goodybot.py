0bace003da5e04f0b0b8e41fc4945b183a771767
Alex Nikanov <alekseynikanov.21@gmail.com>
2017-09-11 21:09:02 +0300

    Remade bug

--- goodybot.py
+++ goodybot.py
@@ -62,7 +62,7 @@ def main():
         last_update_id = last_update['update_id']
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+        #last_chat_name = last_update['message']['chat']['first_name']
 
         message = goodyBot.get_random_message()
 
