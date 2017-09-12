238837927c33649866a725ad88eb8b4a93c3b7b5
Alex Nikanov <alekseynikanov.21@gmail.com>
2017-09-12 09:40:51 +0300

    Fixed bugs

--- goodybot.py
+++ goodybot.py
@@ -45,9 +45,9 @@ def main():
  
         last_update = goodyBot.get_last_update()
  
-        last_update_id = last_update['update_id']
+        last_update_id = int(last_update['update_id'])
         last_chat_text = last_update['message']['text'] or 'None'
-        last_chat_id = last_update['message']['chat']['id'] or 'None'
+        last_chat_id = int(last_update['message']['chat']['id']) or 'None'
         
         if len(allMsg) < LENGTH_MESSAGES:
             if last_chat_text != 'None' and not (last_chat_text in allMsg):
@@ -57,17 +57,20 @@ def main():
             if last_chat_text != 'None' and not (last_chat_text in allMsg):
                 allMsg.append(last_chat_text)
 
-        message = allMsg[random.randint(0, len(allMsg) - 1)]
-
+        if len(allMsg) > 0:
+            message = allMsg[random.randint(0, len(allMsg) - 1)]
+        else:
+            message = 'None'
+        
         if ('goody' in last_chat_text.lower().split() or 'гуди' in last_chat_text.lower().split() or
             'goody ' in last_chat_text.lower() or ' goody' in last_chat_text.lower() or
             'гуди ' in last_chat_text.lower() or ' гуди' in last_chat_text.lower()) and not (last_update_id in used):
             if message == 'None':
                 goodyBot.send_message(last_chat_id, TEST_MESSAGE)
             else:
-                goodyBot.send_message(last_chat_id, message)
-                used.append(last_update_id)
-
+            	goodyBot.send_message(last_chat_id, message)
+            	used.append(last_update_id)
+                
         new_offset = last_update_id + 1
 
 if __name__ == '__main__' :
